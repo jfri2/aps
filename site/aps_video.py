52935927e4ce67e2830e83075d7482ba5fabde8a
jfri2 <jfritz022@gmail.com>
2022-01-01 23:10:52 -0500

    Started working on two camera solution using imutils, also changed resolution of stream to 720p

--- site/aps_video.py
+++ site/aps_video.py
@@ -9,14 +9,27 @@ from utils import *
 
 
 class ApsVideo:
-    def __init__(self):
+    def __init__(self, source='pi'):
+        # source: "pi" for pi camera, "usb" for usb webcam. Must have fswebcam installed on host machine
         self.vs_started = True
         self.frame_lock = threading.Lock()
         self.frame_queue = Queue(max_size=10)
+        
+        if (source == 'pi'):
+            self.source = 0
+        elif (source == 'usb'):
+            self.source = 1
+        else:
+            print('No camera source entered, defaulting to pi camera')
+            self.source = 0
 
         # Init video stream, allow camera to warmup
-        self.vs = VideoStream(src=0).start()
-        time.sleep(2.0)
+        self.frame_width_px = 1280
+        self.frame_height_px = 720
+        self.vs = VideoStream(src=self.source).start()        
+        self.vs.stream.set(3, self.frame_width_px)
+        self.vs.stream.set(4, self.frame_height_px)
+        time.sleep(0.5)
 
         # Init timelapse object
         self.timelapse = Timelapse()
